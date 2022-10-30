09e705f45463e7fba02749da58f1c3ee63cdf48c
jack hsu, 徐効群 <jack23h67@hotmail.com>
2022-10-30 15:47:28 +0800

    2022/10/30 1544 add delete, select all

--- src/main/java/com/example/demo/mapper/StudentAccountMapper.java
+++ src/main/java/com/example/demo/mapper/StudentAccountMapper.java
@@ -1,10 +1,14 @@
 package com.example.demo.mapper;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
 import com.example.demo.model.Student;
+
 
 
 @Mapper
@@ -35,4 +39,13 @@ public interface StudentAccountMapper {
 			  + " WHERE 1=1 "
 			  + "	AND ID = #{id} ")
 		public Integer update(Student student);
+		
+		@Delete("DELETE FROM STUDENT Where id = #{id}")
+		public int deleteDataById(String id);
+		
+		@Select("SELECT * FROM STUDENT")
+		public List<Student> getList();
+
+		@Select("SELECT * FROM STUDENT WHERE id = #{id}")
+		public Student getDataById(String id);
 }
